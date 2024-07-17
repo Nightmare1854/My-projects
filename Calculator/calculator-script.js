92e8e1ef1659a49282d17255df7d0e405e8abaed
Vishwanand G R <143947894+Nightmare1854@users.noreply.github.com>
2024-07-17 11:35:19 +0530

    Add files via upload

---
+++ Calculator/calculator-script.js
@@ -0,0 +1,13 @@
+function displayValue(val) {
+    document.getElementById("display").value = document.getElementById("display").value + val;
+}
+
+function clearDisplay(val) {
+    document.getElementById("display").value = "";
+}
+
+function Calculate() {
+    var userInput = document.getElementById("display").value;
+    var result = eval(userInput);
+    document.getElementById("display").value = result;
+}
